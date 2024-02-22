d088ee4d76f6bb75b1918f77d7a6c2bde94d761d
danny-mak1993 <danny.mak2@gmail.com>
2024-02-21 23:10:33 -0600

    added formingamagicsquare algo

--- src/formingamagicsquare.java
+++ src/formingamagicsquare.java
@@ -8,7 +8,7 @@ vector<string> split(const string &);
 
 /*
  * 
- * We define a magic square to be an nxn matrix of distinct positive integers from 1 to n^2 where the sum of any row, column, or diagonal of length n is always equal to the same number: the magic constant.
+ * We define a magic square to be an n x n matrix of distinct positive integers from 1 to n^2 where the sum of any row, column, or diagonal of length n is always equal to the same number: the magic constant.
 
 You will be given a 3x3 matrix s of integers in the inclusive range |1,9|. We can convert any digit a to any other digit b in the range |1,9| at cost of |a-b|. Given s, convert it into a magic square at minimal cost. Print this cost on a new line.
 
