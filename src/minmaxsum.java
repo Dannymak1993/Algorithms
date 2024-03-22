bc66794e786ce68cc8e48a27866a41a9a76e3fd5
danny-mak1993 <danny.mak2@gmail.com>
2024-03-22 13:36:31 -0500

    added minmax algo solution

--- src/minmaxsum.java
+++ src/minmaxsum.java
@@ -12,8 +12,7 @@ import static java.util.stream.Collectors.toList;
 
 class Result {
 
-    /*
-     * Complete the 'miniMaxSum' function below.
+    /* Complete the 'miniMaxSum' function below.
      * The function accepts INTEGER_ARRAY arr as parameter.
      * Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
      */
