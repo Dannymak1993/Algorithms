3381747f7c1371ae9c86bbabe3fb5795a487f628
danny-mak1993 <danny.mak2@gmail.com>
2024-04-04 21:44:37 -0500

    added birthdaycakecandles algo solution

--- src/birthdaycakecandles.java
+++ src/birthdaycakecandles.java
@@ -12,9 +12,7 @@ import static java.util.stream.Collectors.toList;
 
 class Result {
 
-    /*
-     * Complete the 'birthdayCakeCandles' function below.
-     *
+    /* Complete the 'birthdayCakeCandles' function below.
      * The function is expected to return an INTEGER.
      * The function accepts INTEGER_ARRAY candles as parameter.
      * You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. They will only be able to blow out the tallest of the candles. Count how many candles are tallest.
