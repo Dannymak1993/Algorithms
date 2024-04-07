e577496438bef99feac8ea03156ff4dcb34e3875
danny-mak1993 <danny.mak2@gmail.com>
2024-04-06 22:44:51 -0500

    added appleandorange algorithym solution

--- src/appleorange.java
+++ src/appleorange.java
@@ -22,7 +22,6 @@ class Result {
      *  4. INTEGER b
      *  5. INTEGER_ARRAY apples
      *  6. INTEGER_ARRAY oranges
-     *  
      *  Sam's house has an apple tree and an orange tree that yield an abundance of fruit. Using the information given below, determine the number of apples and oranges that land on Sam's house.
 
 In the diagram below:
