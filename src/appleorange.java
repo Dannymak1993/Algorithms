36e055a6855d0c79d36503fdf31b83b52fd8567b
danny-mak1993 <danny.mak2@gmail.com>
2024-04-13 15:23:39 -0500

    adjusted appleorange algo solution

--- src/appleorange.java
+++ src/appleorange.java
@@ -25,8 +25,7 @@ class Result {
      *  Sam's house has an apple tree and an orange tree that yield an abundance of fruit. Using the information given below, determine the number of apples and oranges that land on Sam's house.
 
 In the diagram below:
-
-The red region denotes the house, where s is the start point, and t is the endpoint. The apple tree is to the left of the house, and the orange tree is to its right.
+The red region denotes the house, where s is the start point, and t is the end point. The apple tree is to the left of the house, and the orange tree is to its right.
 Assume the trees are located on a single point, where the apple tree is at point , and the orange tree is at point .
 When a fruit falls from its tree, it lands d units of distance from its tree of origin along the -axis. *A negative value of d means the fruit fell d units to the tree's left, and a positive value of d means it falls d units to the tree's right. *
      *Given the value of d for m apples and n oranges, determine how many apples and oranges will fall on Sam's house (i.e., in the inclusive range |s,t|)?
