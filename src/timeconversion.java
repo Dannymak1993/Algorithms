c1bba38de46d7eb586c14cbaf67150da855e6e92
danny-mak1993 <danny.mak2@gmail.com>
2024-04-17 00:25:04 -0500

    timeconversion algo solution

--- src/timeconversion.java
+++ src/timeconversion.java
@@ -14,10 +14,8 @@ class Result {
 
     /*
      * Complete the 'timeConversion' function below.
-     *
      * The function is expected to return a STRING.
      * The function accepts STRING s as parameter.
-     * 
      * Given a time in -hour AM/PM format, convert it to military (24-hour) time.
 
 Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
