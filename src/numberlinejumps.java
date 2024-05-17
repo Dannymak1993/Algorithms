e9d1037d169953896de0d16fe6b036b84ffcf859
danny-mak1993 <danny.mak2@gmail.com>
2024-05-17 14:43:13 -0500

    adjusted algo solution

--- src/numberlinejumps.java
+++ src/numberlinejumps.java
@@ -12,8 +12,7 @@ import static java.util.stream.Collectors.toList;
 
 class Result {
 
-    /*
-     * Complete the 'kangaroo' function below.
+    /* Complete the 'kangaroo' function below.
      * The function is expected to return a STRING.
      * The function accepts following parameters:
      *  1. INTEGER x1
